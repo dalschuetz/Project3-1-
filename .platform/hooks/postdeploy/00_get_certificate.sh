57c2f234eb78147bc91769e7b590e7dd5705d897
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2024-12-11 13:33:39 -0700

    domain

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d YOURDOMAINHERE --nginx --agree-tos --email YOUREMAILHERE
+sudo certbot -n -d cannontracker.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dallenschuetzler@gmail.com
